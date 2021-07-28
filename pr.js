5378f5e0e11606f431c3ccd5cca24977b16fdc0d
folkforms <alan.bergin@gmail.com>
2021-07-28 22:46:51 +0100

    Add tests

--- pr.js
+++ pr.js
@@ -2,7 +2,22 @@ const pr = (shell, option, branch) => {
   console.log(`#### pr: option = ${option}`);
   console.log(`#### pr: branch = '${branch}'`);
 
-  return 0;
+  if(option === "start") {
+    shell.exec(`git push --set-upstream origin ${branch}`);
+    shell.exec(`start https://github.com/IBM/spm-ui-upgrade-helper/pull/new/${branch}`);
+    return 0;
+  }
+
+  if(option === "done") {
+    shell.exec("git checkout main");
+    shell.exec("git pull --prune");
+    shell.exec(`git branch --delete ${branch}`);
+    shell.exec("git log --oneline --graph --decorate --all -10");
+    return 0;
+  }
+
+  shell.echo(`ERROR: Unknown option: '${option}'`);
+  return 1;
 }
 
 module.exports = pr;
--- pr.test.js
+++ pr.test.js
@@ -1,3 +1,4 @@
+const shelljs = require("shelljs");
 const { dummyShellJs, failingShellJs } = require("dummy-shells");
 const pr = require("./pr");
 
@@ -5,21 +6,26 @@ beforeEach(() => {
   dummyShellJs._clear();
 });
 
-test.only("when we call 'pr' with 'start' argument it executes the correct commands", () => {
-  const exitCode = pr(dummyShellJs, "start");
+test("when we call 'pr' with 'start' argument it executes the correct commands", () => {
+  const branch = "foo-branch";
+  const exitCode = pr(dummyShellJs, "start", branch);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.echoList).toContain("foo");
-  expect(dummyShellJs.echoList).toContain("bar");
+  expect(dummyShellJs.execList).toContain(`git push --set-upstream origin ${branch}`);
+  expect(dummyShellJs.execList).toContain(`start https://github.com/IBM/spm-ui-upgrade-helper/pull/new/${branch}`);
 });
 
-test("when we call 'pr' with 'done' argument it executes the correct commands", () => {
-  const exitCode = pr(dummyShellJs, props, ["-d"]);
+test.only("when we call 'pr' with 'done' argument it executes the correct commands", () => {
+  const branch = "foo-branch";
+  const exitCode = pr(dummyShellJs, "done", branch);
   expect(exitCode).toEqual(0);
-  expect(dummyShellJs.echoList).toContain("ERROR: Unknown option 'foo'.");
+  expect(dummyShellJs.execList).toContain("git checkout main");
+  expect(dummyShellJs.execList).toContain("git pull --prune");
+  expect(dummyShellJs.execList).toContain(`git branch --delete ${branch}`);
+  expect(dummyShellJs.execList).toContain("git log --oneline --graph --decorate --all -10");
 });
 
-test("when we call 'pr' on the main branch it fails", () => {
-  const exitCode = pr(dummyShellJs, props, ["-d"]);
-  expect(exitCode).toEqual(0);
-  expect(dummyShellJs.echoList).toContain("ERROR: Unknown option 'foo'.");
+test("when we call 'pr' with 'start' argument on the main branch it fails", () => {
+  const exitCode = pr(dummyShellJs, "start", "main");
+  expect(exitCode).toEqual(1);
+  expect(dummyShellJs.echoList).toContain("ERROR: foo");
 });
