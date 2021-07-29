0166a5b9cedc12174e9074405233674d5924613d
folkforms <alan.bergin@gmail.com>
2021-07-29 19:28:08 +0100

    Add dry run option

--- cli.js
+++ cli.js
@@ -3,12 +3,16 @@
 const prx = require("./prx");
 const gitUtils = require("./gitUtils");
 const shelljs = require("shelljs");
+const { dryRunShellJs } = require("dummy-shells");
 const { Command } = require('commander');
 
 const program = new Command();
 program.option('-d, --done', 'Finish PR workflow');
+program.option('-n, --dry-run', 'Dry run');
 program.parse();
 
 const option = program.opts().done ? "done" : "start";
 
-return prx(option, shelljs, gitUtils);
+const shell = program.opts().dryRun ? dryRunShellJs : shelljs;
+
+return prx(option, shell, gitUtils);
